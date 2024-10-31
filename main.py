41b181a2a203b785855eed27f7afde5ecf3644bb
Mak5er <mmaksym.reva@gmail.com>
2024-10-31 11:23:14 +0200

    updated GA url

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def send_analytics(user_id, chat_type, action_name):
     }
     async with ClientSession() as client:
         await client.post(
-            f"https://www.google-analytics.com/mp/collect?measurement_id={MEASUREMENT_ID}&api_secret={API_SECRET}&uid={user_id}",
+            f"https://www.google-analytics.com/mp/collect?measurement_id={MEASUREMENT_ID}&api_secret={API_SECRET}",
             json=params)
 
 
