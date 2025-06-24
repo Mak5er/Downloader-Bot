289acf0db6bf457af5dad2c32710a79061aeb9b3
Mak5er <mmaksym.reva@gmail.com>
2025-06-24 12:29:22 +0300

    Update __init__.py

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,9 +1,7 @@
 from .antiflood import AntifloodMiddleware
 from .ban_middleware import UserBannedMiddleware
-from .subscription_middleware import SubscriptionMiddleware
 
 __all__ = [
     UserBannedMiddleware,
     AntifloodMiddleware,
-    SubscriptionMiddleware,
 ]
