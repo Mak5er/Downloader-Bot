fcd002cbaad20682928e67f03b7f3fb2279ca3ec
mak5er <mmaksym.reva@gmail.com>
2026-02-16 00:47:12 +0200

    chore: remove unused filters

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,9 +1,5 @@
-from .chat_type import ChatTypeF
-from .starts_with import StartsWith
 from .is_bot_admin import IsBotAdmin
 
 __all__ = [
-    'ChatTypeF',
-    'StartsWith',
     'IsBotAdmin'
 ]
--- filters/is_bot_admin.py
+++ filters/is_bot_admin.py
@@ -6,6 +6,5 @@ from aiogram.types import Message
 from config import ADMINS_UID
 
 class IsBotAdmin(BaseFilter):
-    """Перевіряє чи є користувач адміном бота"""
     async def __call__(self, msg: Message) -> bool:
         return msg.from_user.id in ADMINS_UID
